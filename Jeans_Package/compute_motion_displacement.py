a77e9010c8eae1bc97e8920aca7f7d8d5b9c3a1f
jean-ye <sijingye1206@gmail.com>
2020-05-03 20:56:33 -0400

    Updated script and added some tests

--- Jeans_Package/compute_motion_displacement.py
+++ Jeans_Package/compute_motion_displacement.py
@@ -1,14 +1,16 @@
+#Importing relevant packages
 import pandas as pd
 import numpy as np 
 from os import mkdir, chdir, getcwd, path, remove
 
+#Below are the code used to compute displacement mean for each participant 
 def move_into_directory(participant):
     """Move into the directory where each participant's motion file is saved"""
     path = "Motion_files/" + participant
     chdir(path)
     
 def set_motion_file():
-    """Check to make sure motion file exist and set motion file"""
+    """Check to make sure motion file exist and set the motion file to be the one the script wants to operate on"""
     if path.isfile('rp_s_full.txt'):
         column_names = ['roll', 'pitch', 'yaw', 'z', 'x', 'y']
         file = pd.read_csv('rp_s_full.txt', names = column_names, delim_whitespace = True)
@@ -19,23 +21,29 @@ def set_motion_file():
 
 def compute_mean_for_each_column(motion_dataframe):
     """Compute means for each of the six motion parameters"""
-    means = []
+    means_separated = []
     for i in range(6):
         mean = motion_dataframe.iloc[:,i].mean()
-        means.append(mean)
-    np.savetxt('Mean_of_Each_Motion_Parameters', X = means)
-    return means
+        means_separated.append(mean)
+    return means_separated
 
 def compute_mean_of_all_columns(means_separated):
     """Compute the mean of the six motion parameter averages"""
     column_means = sum(means_separated)/6
-    np.savetxt('Mean_of_All_Motion_Parameters', X = [column_means])
     return column_means
 
+def saving_out_text_files_with_averages(means,column_means):
+    """Save the averages computed by the previous two functions in text files"""
+    np.savetxt('Mean_of_Each_Motion_Parameters', X = means)
+    np.savetxt('Mean_of_All_Motion_Parameters', X = [column_means])
+
 def main(participant):
+    """This function puts all the previous functions in the script together"""
     basedir = '/Users/yesji/test_project_folder/project_spring_2020'
     chdir(basedir)
     move_into_directory(participant)
     motion_dataframe = set_motion_file()
     means_separated = compute_mean_for_each_column(motion_dataframe)
     column_means = compute_mean_of_all_columns(means_separated)
+    saving_out_text_files_with_averages(means_separated,column_means)
+    chdir(basedir)
--- tests/motion_displacement_test.py
+++ tests/motion_displacement_test.py
@@ -13,10 +13,23 @@ def test_column_avg2():
     output = [1,2,3,4,5,6]
     assert results == output
 
-def test_avg_sum():
+def test_column_avg3():
+    """This is to make sure the function can handle negative numbers too"""
+    sample_data = {'roll':[0, 1, -4], 'pitch':[2, 2, -10], 'yaw':[-6, 3, 3], 'z':[4, -8, 4], 'x':[5, 5, 5], 'y':[6, 6, 6]}
+    sample_df = pd.DataFrame(data = sample_data)
+    results = compute_mean_for_each_column(sample_df)
+    output = [-1,-2,0,0,5,6]
+    assert results == output
+
+def test_avg_sum1():
     sample_data = {'roll':[1, 1, 1], 'pitch':[2, 2, 2], 'yaw':[3, 3, 3], 'z':[4, 4, 4], 'x':[5, 5, 5], 'y':[6, 6, 6]}
     sample_df = pd.DataFrame(data = sample_data)
     results = compute_mean_for_each_column(sample_df)
     sum_results = compute_mean_of_all_columns(results)
     assert sum_results == 3.5
 
+def test_avg_sum2():
+    sample_data = {'roll':[1, 1, 1], 'pitch':[2, 2, 2], 'yaw':[3, 3, 3], 'z':[4, 4, 4], 'x':[5, 5, 5], 'y':[6, 6, 6]}
+    sample_df = pd.DataFrame(data = sample_data)
+    results = compute_mean_for_each_column(sample_df)
+    assert len(results) == 1
