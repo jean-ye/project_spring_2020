914b85915b8f48d7d479998f9f6731118327c5a5
jean-ye <sijingye1206@gmail.com>
2020-05-03 21:05:37 -0400

    Updated tests

--- tests/motion_displacement_test.py
+++ tests/motion_displacement_test.py
@@ -29,8 +29,9 @@ def test_avg_sum1():
     assert sum_results == 3.5
 
 def test_avg_sum2():
-    sample_data = {'roll':[1, 1, 1], 'pitch':[2, 2, 2], 'yaw':[3, 3, 3], 'z':[4, 4, 4], 'x':[5, 5, 5], 'y':[6, 6, 6]}
+    """This is to make sure the function can handle negative numbers too"""
+    sample_data = {'roll':[0, 1, -4], 'pitch':[2, 2, -10], 'yaw':[-6, 3, 3], 'z':[4, -8, 4], 'x':[3, 3, 3], 'y':[6, 6, 6]}
     sample_df = pd.DataFrame(data = sample_data)
     results = compute_mean_for_each_column(sample_df)
     sum_results = compute_mean_of_all_columns(results)
-    assert len(sum_results) == 1
+    assert sum_results = 1
